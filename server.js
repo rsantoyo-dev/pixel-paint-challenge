30e20fdb4a261b587deebd5d4424938226d05c0c
benwiley3d <ben@3dverse.com>
2023-07-26 16:56:54 -0400

    feat: set up websocket boilerplate

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { WebSocketServer } = require('ws');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
- return res.send('pong');
+  return res.send('pong');
 });
 
 app.get('/', function (req, res) {
@@ -13,3 +14,46 @@ app.get('/', function (req, res) {
 });
 
 app.listen(process.env.PORT || 8080);
+
+const wsServer = new WebSocketServer({ port: 4040 });
+
+wsServer.on('connection', (ws) => {
+  console.log('WS client connected.');
+
+  ws.send(
+    JSON.stringify({
+      messageType: 'hello',
+    })
+  );
+
+  ws.on('message', (data) => {
+    const message = JSON.parse(data.toString());
+
+    console.log('received message', message);
+
+    if (message.messageType === 'hey' && typeof message.data === 'string') {
+      for (const client of wsServer.clients) {
+        client.send(
+          JSON.stringify({
+            messageType: 'hey',
+            data: message.data,
+          })
+        );
+      }
+    } else {
+      ws.send(
+        JSON.stringify({
+          messageType: 'error',
+          data: 'Client sent an unrecognized message format',
+          originalMessage: message,
+        })
+      );
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('WS client disconnected.');
+  });
+
+  ws.onerror = console.error;
+});
--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
-import "./App.css";
+import React, { useEffect, useRef } from 'react';
+import { canvasSize } from './constants';
+import './App.css';
 
 function App() {
+  /**
+   * @type {React.RefObject<HTMLCanvasElement>}
+   * */
+  const canvasRef = useRef(null);
+  const websocketRef = useRef(
+    new WebSocket(`ws://${window.location.hostname}:4040`)
+  );
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return; // should never happen
+
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvasSize.width, canvasSize.height);
+
+    const ws = websocketRef.current;
+
+    ws.onopen = () => {
+      // send a message as soon as the websocket connection is established
+      ws.send(
+        JSON.stringify({
+          messageType: 'hey',
+          data: 'Hello peers!',
+        })
+      );
+    };
+
+    ws.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      switch (message.messageType) {
+        case 'hello':
+          console.log('WebSocket says hello');
+          break;
+        case 'hey':
+          console.log('Message from another client:', message.data);
+          break;
+        case 'error':
+          console.error(message);
+          break;
+        default:
+          console.error('Unrecognized message format from server');
+      }
+    };
+  }, []);
+
   return (
     <div className="app">
       <header>
@@ -11,7 +58,11 @@ function App() {
       </header>
       <main className="main_content">
         <div className="canvas_container">
-          <canvas width={300} height={200} />
+          <canvas
+            ref={canvasRef}
+            width={canvasSize.width}
+            height={canvasSize.height}
+          />
           <div className="delete_me_message">
             TO BE IMPLEMENTED: canvas content (delete this tag).
           </div>
